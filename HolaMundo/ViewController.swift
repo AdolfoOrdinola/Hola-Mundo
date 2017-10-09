150767f7af0f13f9756ccd0f11ce39175312099b
macbookUser <macbookuser@macbookpro-10.local>
2017-10-09 14:44:51 -0500

    Initial Commit

---
+++ HolaMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by macbookUser on 09/10/17.
+//  Copyright © 2017 macbookUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
